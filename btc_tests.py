d6fcbee6163dd6b51b3aba9bd434f63681afbc97
DeepanS206 <deepan.saravanan83@gmail.com>
2019-04-24 17:20:21 -0400

    testing on local machine

--- btc_tests.py
+++ btc_tests.py
@@ -7,8 +7,8 @@ from btc_crawl import rpc_user, rpc_passwd
 
 '''rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332" % (rpc_user, rpc_passwd))'''
 
-address = "1VayNert3x1KzbpzMGt2qdqrAThiRovi8"
+address = "15cE8MYa48tYKdeJg5TEn6VVWuTHeNcZbc"
 addr_obj = BtcAddress.objects(addr=address).first()
-print("used as input length: ", len(addr_obj.used_as_input))
-print("used as output length: ", len(addr_obj.used_as_output))
+#print("used as input length: ", len(addr_obj.used_as_input))
+#print("used as output length: ", len(addr_obj.used_as_output))
 print("neighbors length: ", len(addr_obj.neighbor_addrs))
